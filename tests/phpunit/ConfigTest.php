439a62fe9ad351ff2e5a891549aeafb48b72a190
Ondra Jodas <ondrej.jodas@keboola.com>
2021-06-25 14:01:47 +0200

    test: add ConfigTest

---
+++ tests/phpunit/ConfigTest.php
@@ -0,0 +1,313 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\App\ProjectBackup\Tests;
+
+use Generator;
+use Keboola\App\ProjectBackup\Config\Config;
+use Keboola\App\ProjectBackup\Config\ConfigDefinition;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+class ConfigTest extends TestCase
+{
+
+    /** @dataProvider validConfigDataProvider */
+    public function testValidConfig(
+        array $configArray,
+        array $expectedCredentialsParameters,
+        string $expectedPath,
+        bool $expectedIsUserDefinedCredentials,
+        string $expectedStorageBackendType
+    ): void {
+        $config = new Config($configArray, new ConfigDefinition());
+
+        Assert::assertEquals($expectedCredentialsParameters, $config->getCredentialsParameters());
+        Assert::assertEquals($expectedPath, $config->getPath());
+        Assert::assertEquals($expectedIsUserDefinedCredentials, $config->isUserDefinedCredentials());
+        Assert::assertEquals($expectedStorageBackendType, $config->getStorageBackendType());
+    }
+
+    /** @dataProvider invalidConfigDataProvider */
+    public function testInvalidConfig(array $configArray, string $expectedExceptionMessage): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+        new Config($configArray, new ConfigDefinition());
+    }
+
+    public function validConfigDataProvider(): Generator
+    {
+        yield 'basic-config' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                ],
+            ],
+            [
+                'storageBackendType' => Config::STORAGE_BACKEND_S3,
+            ],
+            '',
+            false,
+            Config::STORAGE_BACKEND_S3,
+        ];
+
+        yield 's3-global-config' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    '#secret_access_key' => 'testAccessKey',
+                    'region' => 'testRegion',
+                    '#bucket' => 'testBucket',
+                ],
+            ],
+            [
+                'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                'access_key_id' => 'testAccessKeyId',
+                '#secret_access_key' => 'testAccessKey',
+                'region' => 'testRegion',
+                '#bucket' => 'testBucket',
+            ],
+            '',
+            false,
+            Config::STORAGE_BACKEND_S3,
+        ];
+
+        yield 'abs-global-config' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    '#accountKey' => 'testAccountKey',
+                    'region' => 'testRegion',
+                ],
+            ],
+            [
+                'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                'accountName' => 'testAccountName',
+                '#accountKey' => 'testAccountKey',
+                'region' => 'testRegion',
+            ],
+            '',
+            false,
+            Config::STORAGE_BACKEND_ABS,
+        ];
+
+        yield 'replace-global-config-s3' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    '#secret_access_key' => 'testAccessKey',
+                    'region' => 'testRegion',
+                    '#bucket' => 'testBucket',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    '#accountKey' => 'testAccountKey',
+                    'region' => 'testRegion',
+                ],
+            ],
+            [
+                'backupId' => 123456,
+                'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                'access_key_id' => 'testAccessKeyId',
+                '#secret_access_key' => 'testAccessKey',
+                'region' => 'testRegion',
+                '#bucket' => 'testBucket',
+            ],
+            '',
+            true,
+            Config::STORAGE_BACKEND_S3,
+        ];
+
+        yield 'replace-global-config-abs' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'backupPath' => 'testPath',
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    '#accountKey' => 'testAccountKey',
+                    'region' => 'testRegion',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    '#secret_access_key' => 'testAccessKey',
+                    'region' => 'testRegion',
+                    '#bucket' => 'testBucket',
+                ],
+            ],
+            [
+                'backupId' => 123456,
+                'backupPath' => 'testPath',
+                'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                'accountName' => 'testAccountName',
+                '#accountKey' => 'testAccountKey',
+                'region' => 'testRegion',
+            ],
+            'testPath',
+            true,
+            Config::STORAGE_BACKEND_ABS,
+        ];
+    }
+
+    public function invalidConfigDataProvider(): Generator
+    {
+        yield 'abs-missing-path' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    '#accountKey' => 'testAccountKey',
+                    'region' => 'testRegion',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                ],
+            ],
+            'Missing required parameter "backupPath".',
+        ];
+
+        yield 'abs-missing-accountName' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'backupPath' => 'testBackupPath',
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    '#accountKey' => 'testAccountKey',
+                    'region' => 'testRegion',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                ],
+            ],
+            'Missing required parameter "accountName".',
+        ];
+
+        yield 'abs-missing-accountKey' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'backupPath' => 'testBackupPath',
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    'region' => 'testRegion',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                ],
+            ],
+            'Missing required parameter "#accountKey".',
+        ];
+
+        yield 'abs-missing-region' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'backupPath' => 'testBackupPath',
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => 'testAccountName',
+                    '#accountKey' => 'testAccountKey',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                ],
+            ],
+            'Missing required parameter "region".',
+        ];
+
+        yield 's3-missing-AccessKeyId' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    '#secret_access_key' => 'testAccessKey',
+                    'region' => 'testRegion',
+                    '#bucket' => 'testBucket',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                ],
+            ],
+            'Missing required parameter "access_key_id".',
+        ];
+
+        yield 's3-missing-secretAccessKey' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    'region' => 'testRegion',
+                    '#bucket' => 'testBucket',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                ],
+            ],
+            'Missing required parameter "#secret_access_key".',
+        ];
+
+        yield 's3-missing-region' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    '#secret_access_key' => 'testAccessKey',
+                    '#bucket' => 'testBucket',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                ],
+            ],
+            'Missing required parameter "region".',
+        ];
+
+        yield 's3-missing-bucket' => [
+            [
+                'action' => 'run',
+                'parameters' => [
+                    'backupId' => 123456,
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => 'testAccessKeyId',
+                    '#secret_access_key' => 'testAccessKey',
+                    'region' => 'testRegion',
+                ],
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                ],
+            ],
+            'Missing required parameter "#bucket".',
+        ];
+    }
+}
