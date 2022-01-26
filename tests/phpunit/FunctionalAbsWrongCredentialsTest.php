709fd4ffde6f5513d3c94deec8332b146fca2894
Výborný Adam <mail@adamvyborny.cz>
2022-01-26 09:39:03 +0100

    COM-1086 tests for error handling when invalid credentials provided for AWS or Azure

---
+++ tests/phpunit/FunctionalAbsWrongCredentialsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\App\ProjectBackup\Tests;
+
+use Keboola\App\ProjectBackup\Config\Config;
+use Keboola\StorageApi\Client as StorageApi;
+use Keboola\Temp\Temp;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class FunctionalAbsWrongCredentialsTest extends TestCase
+{
+    protected Temp $temp;
+
+    protected StorageApi $sapiClient;
+
+    private string $testRunId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->temp = new Temp('project-backup');
+        $this->temp->initRunFolder();
+
+        $this->sapiClient = new StorageApi([
+            'url' => getenv('TEST_AZURE_STORAGE_API_URL'),
+            'token' => getenv('TEST_AZURE_STORAGE_API_TOKEN'),
+        ]);
+
+        $this->testRunId = $this->sapiClient->generateRunId();
+    }
+
+    public function testWrongAccountKey(): void
+    {
+        $fileSystem = new Filesystem();
+        $fileSystem->dumpFile(
+            $this->temp->getTmpFolder() . '/config.json',
+            (string) json_encode([
+                'action' => 'generate-read-credentials',
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_ABS,
+                    'accountName' => getenv('TEST_AZURE_ACCOUNT_NAME'),
+                    '#accountKey' => 'wrongsecret',
+                    'region' => getenv('TEST_AZURE_REGION'),
+                ],
+            ])
+        );
+
+        $runProcess = $this->createTestProcess();
+        $exception = null;
+
+        try {
+            $runProcess->mustRun();
+        } catch (ProcessFailedException $e) {
+            $exception = $e;
+        }
+
+        $this->assertInstanceOf(ProcessFailedException::class, $exception);
+        $this->assertEquals(1, $runProcess->getExitCode());
+
+        $output = $runProcess->getOutput();
+        $errorOutput = $runProcess->getErrorOutput();
+
+        $this->assertEmpty($output);
+        $this->assertRegExp(
+            '/Server failed to authenticate the request. ' .
+            'Make sure the value of Authorization header is formed correctly including the signature./',
+            $errorOutput
+        );
+    }
+
+    private function createTestProcess(): Process
+    {
+        $runCommand = 'php /code/src/run.php';
+        return Process::fromShellCommandline($runCommand, null, [
+            'KBC_DATADIR' => $this->temp->getTmpFolder(),
+            'KBC_URL' => getenv('TEST_AZURE_STORAGE_API_URL'),
+            'KBC_TOKEN' => getenv('TEST_AZURE_STORAGE_API_TOKEN'),
+            'KBC_RUNID' => $this->testRunId,
+        ]);
+    }
+}
---
+++ tests/phpunit/FunctionalS3WrongCredentialsTest.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Keboola\App\ProjectBackup\Tests;
+
+use Keboola\App\ProjectBackup\Config\Config;
+use Keboola\StorageApi\Client as StorageApi;
+use Keboola\Temp\Temp;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class FunctionalS3WrongCredentialsTest extends TestCase
+{
+    protected Temp $temp;
+
+    protected StorageApi $sapiClient;
+
+    private string $testRunId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->temp = new Temp('project-backup');
+        $this->temp->initRunFolder();
+
+        $this->sapiClient = new StorageApi([
+            'url' => getenv('TEST_STORAGE_API_URL'),
+            'token' => getenv('TEST_STORAGE_API_TOKEN'),
+        ]);
+
+        $this->testRunId = $this->sapiClient->generateRunId();
+    }
+
+    public function testWrongSecret(): void
+    {
+        $fileSystem = new Filesystem();
+        $fileSystem->dumpFile(
+            $this->temp->getTmpFolder() . '/config.json',
+            (string) json_encode([
+                'action' => 'generate-read-credentials',
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => getenv('TEST_AWS_ACCESS_KEY_ID'),
+                    '#secret_access_key' => getenv('TEST_AWS_SECRET_ACCESS_KEY') . 'invalid',
+                    'region' => getenv('TEST_AWS_REGION'),
+                    '#bucket' => getenv('TEST_AWS_S3_BUCKET'),
+                ],
+            ])
+        );
+
+        $runProcess = $this->createTestProcess();
+        $exception = null;
+
+        try {
+            $runProcess->mustRun();
+        } catch (ProcessFailedException $e) {
+            $exception = $e;
+        }
+
+        $this->assertInstanceOf(ProcessFailedException::class, $exception);
+        $this->assertEquals(1, $runProcess->getExitCode());
+
+        $output = $runProcess->getOutput();
+        $errorOutput = $runProcess->getErrorOutput();
+
+        $this->assertEmpty($output);
+        $this->assertSame(
+            'The request signature we calculated does not match the signature you provided. ' .
+            "Check your AWS Secret Access Key and signing method. Consult the service documentation for details.\n",
+            $errorOutput
+        );
+    }
+
+    public function testWrongId(): void
+    {
+        $fileSystem = new Filesystem();
+        $fileSystem->dumpFile(
+            $this->temp->getTmpFolder() . '/config.json',
+            (string) json_encode([
+                'action' => 'generate-read-credentials',
+                'image_parameters' => [
+                    'storageBackendType' => Config::STORAGE_BACKEND_S3,
+                    'access_key_id' => getenv('TEST_AWS_ACCESS_KEY_ID') . 'invalid',
+                    '#secret_access_key' => getenv('TEST_AWS_SECRET_ACCESS_KEY'),
+                    'region' => getenv('TEST_AWS_REGION'),
+                    '#bucket' => getenv('TEST_AWS_S3_BUCKET'),
+                ],
+            ])
+        );
+
+        $runProcess = $this->createTestProcess();
+        $exception = null;
+
+        try {
+            $runProcess->mustRun();
+        } catch (ProcessFailedException $e) {
+            $exception = $e;
+        }
+
+        $this->assertInstanceOf(ProcessFailedException::class, $exception);
+        $this->assertEquals(1, $runProcess->getExitCode());
+
+        $output = $runProcess->getOutput();
+        $errorOutput = $runProcess->getErrorOutput();
+
+        $this->assertEmpty($output);
+        $this->assertSame("The security token included in the request is invalid.\n", $errorOutput);
+    }
+
+    private function createTestProcess(): Process
+    {
+        $runCommand = 'php /code/src/run.php';
+        return Process::fromShellCommandline($runCommand, null, [
+            'KBC_DATADIR' => $this->temp->getTmpFolder(),
+            'KBC_URL' => getenv('TEST_STORAGE_API_URL'),
+            'KBC_TOKEN' => getenv('TEST_STORAGE_API_TOKEN'),
+            'KBC_RUNID' => $this->testRunId,
+        ]);
+    }
+
+}
